1c65cd8a21d67e3e0ab40b2e381f1a5104188efe
OTTmann13 <Schweneker.Steffen@googlemail.com>
2014-06-11 18:30:38 +0200

    Finnished Interfaces. Closes #3

---
+++ src/prp_aufgabe4/Container.java
@@ -0,0 +1,5 @@
+package prp_aufgabe4;
+
+public interface Container extends Stowage<Pallet>, WithForm, UniqueID, WithStowLoc, Comparable<Container> {
+
+}
---
+++ src/prp_aufgabe4/Stowage.java
@@ -0,0 +1,6 @@
+
+package prp_aufgabe4;
+
+public interface Stowage <E> extends Mutable, Body, WithCargo, Bounded3DimStack<E> {
+    
+}
